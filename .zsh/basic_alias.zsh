cfbe3d0c7186e474e5eac511d9b1869270a37626
Shun <s.tsuchiya.business@gmail.com>
2023-06-14 01:11:12 +0900

    :technologist: [improve developer experience] update basic alias

--- .zsh/basic_alias.zsh
+++ .zsh/basic_alias.zsh
@@ -1,19 +1,43 @@
-# history
-alias his='history'
+# History
+alias his='history -i'
 
-# cd
+# Change Directory
 alias cd2='cd ../..'
 alias cd3='cd ../../..'
 
-# source
+# Source
 alias so='source'
 
-# ls
+# Show Directory
 alias l='ls -Fh'
 alias la='ls -Fha'
-alias ll='ls -Fhl'
+alias ll='ls -Fhaln'
+alias lls='ls -FhalnS'
 alias lk='ls -Fhk'
 
-# TEXT EDITOR SETTING
+function lsg (){
+  if [ $# -ne 1 ]; then
+    echo "Usage: lsg <pattern>"
+    return 1
+  fi
+  ls -Fh | grep $1
+}
+
+function lscsv () {
+  ls -m > dir.csv
+}
+
+# Text Editor Setting
 alias v='vim'
 alias vi='vim'
+
+# Show Differece of Files
+if [[ -x `which colordiff` ]]; then
+  alias diff='colordiff -u'
+else
+  alias diff='diff -u'
+fi
+
+# Show Amount of Used Resource
+alias df='df -kh'
+alias du='du -kh'
