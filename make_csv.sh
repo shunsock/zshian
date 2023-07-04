ed8e517adee468484ded0c8d921fdbc9027af662
Shun <s.tsuchiya.business@gmail.com>
2023-07-05 01:39:52 +0900

    :necktie: [add business logic] =>  add show_git_alias command

---
+++ make_csv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Remove lines starting with '#'
+grep -v '^#' .zsh/git_alias.zsh | \
+# Remove 'alias ' at the start of each line
+perl -pe 's/^alias //' | \
+# Replace '=' with ','
+perl -pe "s/='/,/" | \
+# Remove trailing quote
+perl -pe "s/'$//" | \
+# Remove blank lines
+grep -v '^$' > .zsh/doc/git_alias.csv
