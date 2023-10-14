a7c34ea6583dc39726365a677a0e3d79b961e4e0
Shun <s.tsuchiya.business@gmail.com>
2023-10-15 02:35:01 +0900

    git promptの修正

--- .zsh/brew/basic.zsh
+++ .zsh/brew/basic.zsh
@@ -2,12 +2,9 @@
 # * if brew has already installed, below lines will be run when reading this file
 # if type brew          : check wheather brew command exist
 # &>                    : redirect both of standard output and error to /dev/null
-# zsh-git-prompt/zshrc  : to use for showing git branch in command prompt
-# autoload -Uz          : prevent from being over written by user alias. use zsh format
 if type brew &>/dev/null; then
   FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
   source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-  source $(brew --prefix)/opt/zsh-git-prompt/zshrc.sh
   autoload -Uz compinit && compinit
   autoload -Uz colors && colors
   autoload -Uz add-zsh-hook
@@ -18,37 +15,45 @@ fi
 ZLE_REMOVE_SUFFIX_CHARS=$'' # add space after completion
 
 
-# PROMPT SETTING #
-# alias python="python3"  : add alias for git_super_status
-#                         : if Mac OS version < 12 Monterey, you can comment out this line
-# %F{color} ... %f        : change color
-# %n                      : user name
-# ($(arch))               : system architecture
-# %D                      : YYYY-mm-dd
-# %T                      : hh:mm
-# $(git_super_status)     : git status
-# %#                      : show "#" if you are root, else show %
-# if brew list | grep ~   : if ~ exists, return true
-# add-zsh-hook precmd     : if
+function ps_comp() {
+  # main input line bar
+  text=$1
+  color=$2
+  bg_color=$3
 
-alias python="python3"
-git_prompt() {
-  newline=$'\n'
-  user_name="%F{green}%n:%f"
-  system_architecture="%F{cyan}⚙️ ($(arch));%f"
-  date_time="%F{yellow}%D %T %f"
-  current_directory="%F{blue}%~%f"
-  git_status="%F{blue}git%f$(git_super_status)"
-  endline="%F{green}↣ %f%# "
+  # triangle
+  tri_color=$4
+  tri_bg_color=$5
+  triangle=$'\ue0b0'
 
-  # if .git exists show it
-  if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = true ]; then
-    PROMPT="$user_name $system_architecture $date_time $newline""$current_directory $git_status $endline"
-  else
-    PROMPT="$user_name $system_architecture $date_time $newline""$current_directory $endline"
+  echo "%F{$color}%K{$bg_color}$text%k%f%F{$tri_color}%K{$tri_bg_color}$triangle%f%k"
+}
+
+function git_branch_name()
+{
+  branch_symbol=$'\ue0a0'
+  git_branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+  if [[ $git_branch != "" ]]; then
+    echo " $branch_symbol $git_branch "
   fi
 }
 
-if brew list | grep zsh-git-prompt &> /dev/null; then
-  add-zsh-hook precmd git_prompt
+
+zsh_prompt() {
+  newline=$'\n\n'
+  gear=$'\u2699'
+
+  # ps_comp "text" "text_color" "bg_color" "tri_color" "tri_bg_color"
+  cwd_path="%{$(ps_comp " %~ " "black" "green" "green" "black")%}"
+  system_arch="%{$(ps_comp " $gear $(arch) " "white" "black" "black" "blue")%}"
+  git_status="%{$(ps_comp "$(git_branch_name)" "black" "blue" "blue" "none")%}"
+
+  second_line="$newline(%F{magenta}%n%f) %F{green}↣ %f%#"
+
+  PROMPT="${cwd_path}${system_arch}${git_status}${second_line}"
+}
+
+
+if brew list &> /dev/null; then
+  add-zsh-hook precmd zsh_prompt
 fi
