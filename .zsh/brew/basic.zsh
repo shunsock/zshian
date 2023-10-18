74a20ca3a0b6cda8ab476aa6487a5f0997c03908
Shun <s.tsuchiya.business@gmail.com>
2023-10-18 15:18:33 +0900

    :recycle: [refactor code] =>  promptを調整しました

--- .zsh/brew/basic.zsh
+++ .zsh/brew/basic.zsh
@@ -19,14 +19,8 @@ function ps_comp() {
   # main input line bar
   text=$1
   color=$2
-  bg_color=$3
 
-  # triangle
-  tri_color=$4
-  tri_bg_color=$5
-  triangle=$'\ue0b0'
-
-  echo "%F{$color}%K{$bg_color}$text%k%f%F{$tri_color}%K{$tri_bg_color}$triangle%f%k"
+  echo "%F{$color}$text%f"
 }
 
 function git_branch_name()
@@ -34,25 +28,30 @@ function git_branch_name()
   branch_symbol=$'\ue0a0'
   git_branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
   if [[ $git_branch != "" ]]; then
-    echo " $branch_symbol $git_branch "
+    echo "$branch_symbol $git_branch"
   fi
 }
 
 
 zsh_prompt() {
-  newline=$'\n\n'
+  newline=$'\n'
   gear=$'\u2699'
+  hacker='shunsock'
+  darkblue='#294C7A'
+  blue='#4DABFF'
+  yellow='#E1E60B'
+  green='#43BFA0'
 
-  # ps_comp "text" "text_color" "bg_color" "tri_color" "tri_bg_color"
-  cwd_path="%{$(ps_comp " %~ " "black" "green" "green" "black")%}"
-  system_arch="%{$(ps_comp " $gear $(arch) " "white" "black" "black" "blue")%}"
-  git_status="%{$(ps_comp "$(git_branch_name)" "black" "blue" "blue" "none")%}"
+  # first line
+  name="%{$(ps_comp "$hacker" "$darkblue")%}"
+  system_arch="%{$(ps_comp "$gear $(arch)" "$blue")%}"
+  git_status="%{$(ps_comp "$(git_branch_name)" "$green")%}"
 
-  second_line="$newline(%F{magenta}%n%f) %F{green}↣ %f%#"
+  # second line
+  second_line="$newline%F{$yellow}%~%f ↣ %# "
 
-  PROMPT="${cwd_path}${system_arch}${git_status}${second_line}"
+  PROMPT="${name} on ${system_arch} on ${git_status}${second_line}"
 }
-
 
 if brew list &> /dev/null; then
   add-zsh-hook precmd zsh_prompt
